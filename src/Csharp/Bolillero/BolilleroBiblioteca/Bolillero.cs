db684ea20f01403e67e3ca7a83e64036d477d821
leonefrain86 <leonefrain86cbo@gmail.com>
2020-04-03 00:39:31 -0300

    Actualizado
    -Creacion de las clases Bolillero y Bolilla
    -Creacion de los metodos Jugar, JugarNveces, delvolverBolillas y sacarBolilla

---
+++ src/Csharp/Bolillero/BolilleroBiblioteca/Bolillero.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BolilleroBiblioteca
+{
+    public class Bolillero
+    {
+        public List<Bolilla> bolillas {get; set;}
+
+        public Bolillero()
+        {
+            this.bolillas = new List<Bolilla>{
+                new Bolilla("rojo", 0),
+                new Bolilla("azul", 1),
+                new Bolilla("verde", 2),
+                new Bolilla("negro", 3),
+                new Bolilla("naranja", 4),
+                new Bolilla("violeta", 5),
+                new Bolilla("amarillo", 6),
+                new Bolilla("marón", 7),
+                new Bolilla("griz", 8),
+                new Bolilla("rosado", 9)
+            };
+        }
+
+        public Bolilla sacarBolilla()
+        {
+            Bolilla bolilla =  this.bolillas[new Random().Next(bolillas.Count())];
+            bolillas.Remove(bolilla);
+            return bolilla;
+        }
+
+        public void devolverBolillas(List<Bolilla> bolillas)
+        {
+            foreach (var bolilla in bolillas)
+            {
+                this.bolillas.Add(bolilla);
+            }
+        }
+
+        public bool unaJugada (List<Bolilla> bolillas)
+        {
+            int comprobador = 0;
+            List<Bolilla> bolillasSacadas = new List<Bolilla>(); //Cree una lista ya que es mas logico que se devuelvan todas las bolillas al terminar un juego.
+            foreach (var bolilla in bolillas)
+            {
+                bolillasSacadas.Add(sacarBolilla());
+                if(bolillasSacadas.Last().num == bolilla.num)
+                {
+                    comprobador++;
+                }
+            }
+            devolverBolillas(bolillasSacadas);
+            return comprobador == bolillas.Count();
+        }
+
+        public int jugarNVeces (List<Bolilla> bolillas, int num)
+        {
+            int contador = 0;
+            for (int i = 0; i < num; i++)
+            {
+                if (unaJugada(bolillas))
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+    }
+}
---
+++ src/Csharp/Bolillero/BolilleroConsola/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BolilleroBiblioteca;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BolilleroConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bolillero unBolillero = new Bolillero();
+
+            // SOLO SE USAN 5 BOLILLAS PARA JUGAR
+            List<Bolilla> bolillas = new List<Bolilla>{
+                new Bolilla("rojo", 0),
+                new Bolilla("azul", 1),
+                new Bolilla("verde", 5),
+                new Bolilla("negro", 3),
+                new Bolilla("naranja", 7)
+            };
+            
+            // CASO DE UNA SOLA JUGADA
+            Console.WriteLine($"Gano: {unBolillero.unaJugada(bolillas)}");
+            informeBolillero(unBolillero);
+
+            // CASO DE MAS DE UNA JUGADA
+            Console.WriteLine($"Usted gano: {unBolillero.jugarNVeces(bolillas,1000000)} veces");
+            informeBolillero(unBolillero);
+        }
+        static void informeBolillero(Bolillero bolillero)
+        {
+            foreach (var bolilla in bolillero.bolillas)
+            {
+                Console.WriteLine($"color: {bolilla.color} num: {bolilla.num}");
+            } 
+        }
+    }
+}
