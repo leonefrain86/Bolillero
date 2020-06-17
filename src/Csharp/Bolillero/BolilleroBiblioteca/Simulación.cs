cae8d5124cedaf40b577620f47a311789eb45288
leonefrain86 <leonefrain86cbo@gmail.com>
2020-06-17 14:47:45 -0300

    Actualizado
    Comenzando con la tercera interacion de Bolillero:
    -Cree un nuevo metodo del tipo Async
    -Modife el diagrama de clases

--- src/Csharp/Bolillero/BolilleroBiblioteca/Simulación.cs
+++ src/Csharp/Bolillero/BolilleroBiblioteca/Simulación.cs
@@ -31,5 +31,24 @@ namespace BolilleroBiblioteca
             return tareas.Sum(x => x.Result);
         }
         
+        public async Task<int> SimularConHilosAsync(Bolillero unBolillero, List<int> unaJugada, int cntSimulaciones, int cntHilos)
+        {
+            Task<int>[] tareas = new Task<int>[cntHilos];
+            int restoSXH = cntSimulaciones % cntHilos;
+
+            for (int i = 0; i < cntHilos; i++)
+            {
+                var clon = (Bolillero)unBolillero.Clone();
+                int simulacionesXH = cntSimulaciones / cntHilos + 1;
+                if (i >= restoSXH)
+                {
+                    simulacionesXH = cntSimulaciones / cntHilos;
+                }
+                tareas[i] = Task.Run(() => clon.jugarNVeces(unaJugada, simulacionesXH));
+            }
+            await Task<int>.WhenAll(tareas);
+
+            return tareas.Sum(x => x.Result);  
+        }
     }
 }
--- src/Csharp/Bolillero/BolilleroConsola/Program.cs
+++ src/Csharp/Bolillero/BolilleroConsola/Program.cs
@@ -1,39 +1,44 @@
 ﻿using System;
 using BolilleroBiblioteca;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
 
 namespace BolilleroConsola
 {
     class Program
     {
+        // static async Task Main(string[] args)
         static void Main(string[] args)
         {
             DateTime FH;
-
+            
             Bolillero unBolillero = new Bolillero(10);
-
-            informeBolillero(unBolillero);
-
             List<int> bolillas = new List<int> { 0, 1, 4};
+            Simulación sml = new Simulación();
 
-            Simulación s = new Simulación();
+            //No devuelve el resultado esperado
+            Console.WriteLine(sml.SimularConHilosAsync(unBolillero, bolillas, 10000000, 3));
+
+            // Hago uso de await pero se bloquea el resto de los procesos
+            // Console.WriteLine(await sml.SimularConHilosAsync(unBolillero, bolillas, 10000000, 3));  
 
             FH = DateTime.Now;
-            Console.WriteLine(s.simularSinHilos(unBolillero, bolillas, 10000000));
+            Console.WriteLine(sml.simularSinHilos(unBolillero, bolillas, 10000000));
             Console.WriteLine($"Duración sin hilos: {DateTime.Now - FH}");
 
             FH = DateTime.Now;
-            Console.WriteLine(s.simularConHilos(unBolillero, bolillas, 10000000, 3));
+            Console.WriteLine(sml.simularConHilos(unBolillero, bolillas, 10000000, 3));
             Console.WriteLine($"Duración con hilos: {DateTime.Now - FH}");
 
+            // Una idea que tuve pero no funcionó ya que eran diferentes tipos de metodos
+            // await Task.WhenAll(simulacion.simularConHilos(), simulacion.SimularConHilosAsync(), simulacion.simularSinHilos())
+            
             // // CASO DE UNA SOLA JUGADA
             // Console.WriteLine($"Gano: {unBolillero.unaJugada(bolillas)}");
 
             // // CASO DE MAS DE UNA JUGADA
             // Console.WriteLine($"Usted gano: {unBolillero.jugarNVeces(bolillas,10000000)} veces");
-            informeBolillero(unBolillero);
-        
         }
         static void informeBolillero(Bolillero bolillero)
         {
