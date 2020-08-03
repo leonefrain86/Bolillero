566a1dce6a70ed797ef39c638832d4f12d92e393
leonefrain86 <leonefrain86cbo@gmail.com>
2020-08-03 13:52:48 -0300

    Actualizado
    -Comenze con la cuarta iteracion del trabajo
    -Hize uso de Parallel.For(...)
    -Modifique el diagrama de clases

--- src/Csharp/Bolillero/BolilleroBiblioteca/Simulación.cs
+++ src/Csharp/Bolillero/BolilleroBiblioteca/Simulación.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+// using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using System.Linq;
 
@@ -48,6 +49,58 @@ namespace BolilleroBiblioteca
             await Task<int>.WhenAll(tareas);
 
             return tareas.Sum(x => x.Result);  
+        }
+
+        public async Task<int> SimularParallelAsync (Bolillero unBolillero, List<int> unaJugada,
+                                                      int cntSimulaciones, int cntHilos)
+        {
+            Task<int>[] tareas = new Task<int>[cntHilos]; // Coleccion de task que cuando finaliza devuelve int
+            // ConcurrentBag<int> tareas2 = new ConcurrentBag<int>[cntHilos]; // Coleccion segura para hilos
+            List<int> tareasList = new List<int>();
+
+            int restoSXH = cntSimulaciones % cntHilos;
+
+
+            await Task.Run(() => Parallel.For(0, cntHilos, i => {
+                var clon = (Bolillero)unBolillero.Clone();
+                int simulacionesXH = cntSimulaciones / cntHilos + 1;
+                if (i >= restoSXH)
+                {
+                    simulacionesXH = cntSimulaciones / cntHilos;
+                }
+                tareasList.Add(clon.jugarNVeces(unaJugada, simulacionesXH));
+            }));
+
+            // Parallel.ForEach(tareas, tarea => {
+            //     var clon = (Bolillero)unBolillero.Clone();
+                
+            //     tarea = Task.Run(() => clon.jugarNVeces(unaJugada, cntSimulaciones/tareas.Count()));
+                
+            // });
+
+            // Parallel.ForEach(tareas, x => x.Start());
+
+            // Parallel.For(0, words.Length, i => results[i] = AddB(words[i]));
+
+            // Parallel.For(inicio, fin, accion);
+            // Parallel.ForEach(Lista, accion);
+
+            // int restoSXH = cntSimulaciones % cntHilos;
+
+            // for (int i = 0; i < cntHilos; i++)
+            // {
+            //     var clon = (Bolillero)unBolillero.Clone();
+            //     int simulacionesXH = cntSimulaciones / cntHilos + 1;
+            //     if (i >= restoSXH)
+            //     {
+            //         simulacionesXH = cntSimulaciones / cntHilos;
+            //     }
+            //     tareas[i] = Task.Run(() => clon.jugarNVeces(unaJugada, simulacionesXH));
+            // }
+            // Parallel.ForEach(tareas, x => x.Start());
+            
+
+            return tareasList.Sum();
         }
     }
 }
--- src/Csharp/Bolillero/BolilleroConsola/Program.cs
+++ src/Csharp/Bolillero/BolilleroConsola/Program.cs
@@ -12,26 +12,32 @@ namespace BolilleroConsola
             DateTime FH;
             
             Bolillero unBolillero = new Bolillero(10);
-            List<int> bolillas = new List<int> { 0, 1, 4};
+            List<int> bolillas = new List<int> { 1, 5, 3};
             Simulación sml = new Simulación();
 
             FH = DateTime.Now;
-            Console.WriteLine(sml.simularSinHilos(unBolillero, bolillas, 100000000));
+            Console.WriteLine(sml.simularSinHilos(unBolillero, bolillas, 10000000));
             Console.WriteLine($"Duración sin hilos: {DateTime.Now - FH}");
 
             FH = DateTime.Now;
-            Console.WriteLine(sml.simularConHilos(unBolillero, bolillas, 100000000, 3));
+            Console.WriteLine(sml.simularConHilos(unBolillero, bolillas, 10000000, 3));
             Console.WriteLine($"Duración con hilos: {DateTime.Now - FH}");
 
             FH = DateTime.Now;
-            Console.WriteLine(await sml.SimularConHilosAsync(unBolillero, bolillas, 100000000, 3));
+            Console.WriteLine(await sml.SimularConHilosAsync(unBolillero, bolillas, 10000000, 3));
             Console.WriteLine($"Duración con hilos y Async: {DateTime.Now - FH}");
+
+            FH = DateTime.Now;
+            Console.WriteLine(await sml.SimularParallelAsync(unBolillero, bolillas, 10000000, 3));
+            Console.WriteLine($"Duración con Parallel y Async: {DateTime.Now - FH}");
             
             // // CASO DE UNA SOLA JUGADA
             // Console.WriteLine($"Gano: {unBolillero.unaJugada(bolillas)}");
 
             // // CASO DE MAS DE UNA JUGADA
             // Console.WriteLine($"Usted gano: {unBolillero.jugarNVeces(bolillas,10000000)} veces");
+
+            // Console.WriteLine(50 / 4);
         }
         static void informeBolillero(Bolillero bolillero)
         {
